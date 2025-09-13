78af3ebfb3c6daf90b5d7657c56ac8c81073ae57
Seruhio30 <sh0041314@gmail.com>
2025-09-12 21:35:14 -0600

    week2 team activity

--- src/js/ProductDetails.mjs
+++ src/js/ProductDetails.mjs
@@ -2,34 +2,33 @@ import { getLocalStorage, setLocalStorage } from "./utils.mjs";
 
 export default class ProductDetails {
 
-    constructor(productId, dataSource) {
-        this.productId = productId;
-        this.product = {};
-        this.dataSource = dataSource;
-    }
+  constructor(productId, dataSource) {
+    this.productId = productId;
+    this.product = {};
+    this.dataSource = dataSource;
+  }
 
-    async init() {
-        this.product = await this.dataSource.findProductById(this.productId);
+  async init() {
+    // use the datasource to get the details for the current product. findProductById will return a promise! use await or .then() to process it
+    this.product = await this.dataSource.findProductById(this.productId);
+    // the product details are needed before rendering the HTML
+    this.renderProductDetails();
+    // once the HTML is rendered, add a listener to the Add to Cart button
+    // Notice the .bind(this). This callback will not work if the bind(this) is missing. Review the readings from this week on 'this' to understand why.
+    document
+      .getElementById('addToCart')
+      .addEventListener('click', this.addProductToCart.bind(this));
+  }
 
-        this.renderProductDetails();
+  addProductToCart() {
+    const cartItems = getLocalStorage("so-cart") || [];
+    cartItems.push(this.product);
+    setLocalStorage("so-cart", cartItems);
+  }
 
-        document
-            .getElementById('addToCart')
-            .addEventListener('click', this.addProductToCart.bind(this));
-    }
-
-    addProductToCart() {
-        const cartItems = getLocalStorage("so-cart") || [];
-        cartItems.push(this.product);
-        setLocalStorage("so-cart", cartItems);
-    }
-
-    renderProductDetails() {
-        productDetailsTemplate(this.product);
-    }
-
-
-
+  renderProductDetails() {
+    productDetailsTemplate(this.product);
+  }
 }
 
 function productDetailsTemplate(product) {
@@ -46,3 +45,5 @@ function productDetailsTemplate(product) {
 
   document.getElementById('addToCart').dataset.id = product.Id;
 }
+
+      
--- src/js/product.js
+++ src/js/product.js
@@ -1,9 +1,14 @@
 import ProductData from './ProductData.mjs';
 import { getParam } from './utils.mjs';
-import ProductDetails from './ProductDetails.mjs';
+import ProductDetails from './productDetails.mjs';
 
 const productId = getParam('product');
 const dataSource = new ProductData('tents');
+
+//solo para prueba
+//dataSource.findProductById(productId).then(product => {
+//console.log(product);
+//});
 
 const product = new ProductDetails(productId, dataSource);
 product.init();
