6928bdbdec6639565e4d09538942d50dafb53588
Seruhio30 <sh0041314@gmail.com>
2025-09-12 20:40:27 -0600

    test1

--- src/js/product.js
+++ src/js/product.js
@@ -1,10 +1,11 @@
-import { setLocalStorage } from './utils.mjs';
+import { getLocalStorage, setLocalStorage } from './utils.mjs';
 import ProductData from './ProductData.mjs';
 
 const dataSource = new ProductData('tents');
 
 function addProductToCart(product) {
-  setLocalStorage('so-cart', product);
+  const cartItems = getLocalStorage("so-cart") || []; 
+  setLocalStorage("so-cart", cartItems);
 }
 // add to cart button event handler
 async function addToCartHandler(e) {
