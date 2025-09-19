48daa6facbaeb76919986aa4325b612e361e1c07
laura nunez <laupao41@gmail.com>
2025-09-19 17:00:49 +0900

    Arreglado Add to Cart: contador funciona, suma de 1 por clic y se actualiza instantáneamente

--- src/js/product.js
+++ src/js/product.js
@@ -3,40 +3,35 @@
 import ProductData from './ProductData.mjs';
 import { getParam, getLocalStorage, setLocalStorage, updateCartCount } from './utils.mjs';
 import ProductDetails from './ProductDetails.mjs';
-
-
 import { loadHeaderFooter } from './utils.mjs';
-loadHeaderFooter();
-updateCartCount();
+
+
+
+document.addEventListener("DOMContentLoaded", async () => {
+  if (document.querySelector("#main-header") && document.querySelector("#main-footer")) {
+    await loadHeaderFooter(); // <-- aquí usas await
+ 
+   
+      updateCartCount();
+    }
 
 const productId = getParam('product');
 const dataSource = new ProductData('tents');
-
-//solo para prueba
-//dataSource.findProductById(productId).then(product => {
-//console.log(product);
-//});
-
-function addProductToCart(product) {
-
-
-  let cartItems = getLocalStorage('so-cart');
-
-  // me seguro de que sea un array
-  if (!Array.isArray(cartItems)) {
-    cartItems = [];
-  }
-
-  cartItems.push(product);
-  setLocalStorage('so-cart', cartItems);
-
-}
-
-// add to cart button event handler
-async function addToCartHandler(e) {
-  const product = dataSource.findProductById(e.target.dataset.id);
-  addProductToCart(product);
-}
-
 const productDetails = new ProductDetails(productId, dataSource);
-productDetails.init();
+await productDetails.init();
+
+
+const addToCartBtn = document.querySelector('#addToCart');
+  if (addToCartBtn) {
+    addToCartBtn.replaceWith(addToCartBtn.cloneNode(true));
+    const newBtn = document.querySelector('#addToCart');
+    newBtn.addEventListener('click', async (e) => {
+      const product = await dataSource.findProductById(e.target.dataset.id);
+      let cartItems = getLocalStorage('so-cart');
+      if (!Array.isArray(cartItems)) cartItems = [];
+      cartItems.push(product);
+      setLocalStorage('so-cart', cartItems);
+      updateCartCount();
+    });
+  }
+});
