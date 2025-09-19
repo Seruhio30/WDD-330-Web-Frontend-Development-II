a7a945242f44b74ae82bfa52dec20b2c17a8e14b
Seruhio30 <sh0041314@gmail.com>
2025-09-19 14:55:19 -0600

    can

--- src/js/product.js
+++ src/js/product.js
@@ -1,27 +1,19 @@
-
-
 import ProductData from './ProductData.mjs';
-import { getParam, getLocalStorage, setLocalStorage, updateCartCount } from './utils.mjs';
+import { getParam, getLocalStorage, setLocalStorage, updateCartCount, loadHeaderFooter } from './utils.mjs';
 import ProductDetails from './ProductDetails.mjs';
-import { loadHeaderFooter } from './utils.mjs';
-
-
 
 document.addEventListener("DOMContentLoaded", async () => {
   if (document.querySelector("#main-header") && document.querySelector("#main-footer")) {
-    await loadHeaderFooter(); // <-- aquí usas await
- 
-   
-      updateCartCount();
-    }
+    await loadHeaderFooter();
+    updateCartCount();
+  }
 
-const productId = getParam('product');
-const dataSource = new ProductData('tents');
-const productDetails = new ProductDetails(productId, dataSource);
-await productDetails.init();
+  const productId = getParam('product');
+  const dataSource = new ProductData('tents');
+  const productDetails = new ProductDetails(productId, dataSource);
+  await productDetails.init();
 
-
-const addToCartBtn = document.querySelector('#addToCart');
+  const addToCartBtn = document.querySelector('#addToCart');
   if (addToCartBtn) {
     addToCartBtn.replaceWith(addToCartBtn.cloneNode(true));
     const newBtn = document.querySelector('#addToCart');
